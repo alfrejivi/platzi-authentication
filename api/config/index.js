6083b390b479122100ebdf6b49e3e90b9f118e4e
Miguel Angel <devmiguelangel@gmail.com>
2018-12-28 12:35:19 -0400

    client credentials grant

--- api/config/index.js
+++ api/config/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const config = {
   authJwtSecret: process.env.AUTH_JWT_SECRET,
+  spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
+  spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
 };
 
 module.exports = { config: config };
